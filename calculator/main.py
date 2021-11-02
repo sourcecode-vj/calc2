03f12c834707c28320054ebf11d3519442f545eb
sourcecode-vj <vj6@njit.edu>
2021-11-02 12:39:15 -0400

    Updated for 601

--- calculator/main.py
+++ calculator/main.py
@@ -21,3 +21,7 @@ class Calculator:
         """ multiply two numbers and store the result"""
         self.result = value_a * value_b
         return self.result
+    @staticmethod
+    def divide_numbers(value_a, value_b):
+        """ divide two numbers and store the result"""
+        return value_a / value_b
